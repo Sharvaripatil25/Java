09a5b56a8f27e02f0ec24f29a6ba29344c98abbb
Sharvaripatil25 <sharvaripatil333@gmail.com>
2025-10-28 14:09:52 +0530

    new file

--- Lambda.java
+++ Lambda.java
@@ -4,11 +4,11 @@
 //Syntax:
 //(parameters)->{body}
 interface Test{
-    void display();
+    void add(int a,int b);
 }
 class abc implements Test{
-    public void display(){
-        System.out.println("Hello");
+    public void add(int a,int b){
+        System.out.println(a+b);
     }
 }
 
@@ -16,9 +16,9 @@ public class Lambda {
     public static void main(String[] args) {
         //using normal way
         Test t1=new abc();
-        t1.display();
+        t1.add(3,4);
         //using lambda expression
-        Test t2=()->{System.out.println("Hello from lambda");};
-        t2.display();
+        Test t2=(a,b)->{System.out.println(a+b);};
+        t2.add(4,5);
     }
 }
